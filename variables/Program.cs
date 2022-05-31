b646701a3a1336e530eb08f51e5c34ff7abc9db0
jermaine buford <bufordjermaine@outlook.com>
2022-05-31 14:54:48 -0400

    intial commit

---
+++ variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+
+            bool isAlive;
+
+            age = 24;
+
+            name = "jermaine";
+
+            isAlive = true;
+
+            Console.WriteLine(  $"my name is {name}. i am {age} years old.");
+        }
+        
+    }
+}
